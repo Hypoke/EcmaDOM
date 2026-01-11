dcafc5336f29e5aeb633e187dac0b4f207801e55
FriedrichXLucas <friedrich.lucas@tuorsoft.com>
2026-01-11 01:28:23 +0100

    :bug: Fix publish pipeline

---
+++ assets/system/bstypes.d.ts
@@ -0,0 +1,11 @@
+declare namespace A11y {
+}
+
+declare namespace CSSParser {
+}
+
+declare namespace CSSStyleDeclaration {
+}
+
+declare namespace HTMLParser {
+}
--- mod.js
+++ mod.js
@@ -1,3 +1,5 @@
+/* @ts-self-types="./assets/system/bstypes.d.ts" */
+
 export { A11y } from "./src/A11y.class.js";
 export { CSSParser } from "./src/CSSParser.class.js";
 export { CSSStyleDeclaration } from "./src/CSSStyleDeclaration.class.js";
